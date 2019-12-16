3842751293a151137891253410f58a98c4c09a17
omarbradai <bradaiomar1@gmail.com>
2019-12-17 00:37:41 +0100

    allow cross origin

--- src/main/java/ob/hb/tunisiejobing/rest/ServiceController.java
+++ src/main/java/ob/hb/tunisiejobing/rest/ServiceController.java
@@ -6,12 +6,14 @@ import ob.hb.tunisiejobing.utils.DtoEntityMapper;
 import ob.hb.tunisiejobing.service.IServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("services")
 public class ServiceController implements Resource<TjService, TjServiceDto> {
